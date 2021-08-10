ee7f7b9eb52d2eb248cae280bd546bcb29a19cbd
katiandrews <andryushina.eka@gmail.com>
2021-08-10 18:43:27 +0200

    feat: add active route in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,35 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import s from "./Header.css";
 
 export function Header() {
+  const [activeRoute, setActiveRoute] = useState("home");
+
   return (
     <header className={s.header}>
       <nav>
         <ul className={s.nav}>
-          <li>
-            <Link className={s.item} to="/">
+          <li
+            onClick={() => {
+              setActiveRoute("home");
+            }}
+          >
+            <Link
+              className={`${s.item} ${activeRoute === "home" ? s.active : ""}`}
+              to="/"
+            >
               Home
             </Link>
           </li>
-          <li>
-            <Link className={s.item} to="/about">
+          <li
+            onClick={() => {
+              setActiveRoute("about");
+            }}
+          >
+            <Link
+              className={`${s.item} ${activeRoute === "about" ? s.active : ""}`}
+              to="/about"
+            >
               About
             </Link>
           </li>
